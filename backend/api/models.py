285a2a50fa7758f633759ab56db379004752a137
Suvigya Bajpai <49677003+Suvigya96@users.noreply.github.com>
2024-07-09 13:44:45 -0500

    Update models.py

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,15 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Comment(models.Model):
-    comment_owner_username = models.CharField(max_length=50)
-    posted_at = models.DateTimeField()
-    comment = models.TextField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
-    
-    def __str__(self):
-        return self.username
-
 class VideoUrl(models.Model):
     videourl = models.URLField(max_length=200)
     
