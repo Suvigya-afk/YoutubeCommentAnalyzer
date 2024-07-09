bf8d87daf2942139ab678eead354b7e75353bb9b
Suvigya Bajpai <49677003+Suvigya96@users.noreply.github.com>
2024-07-09 13:45:06 -0500

    Update serializers.py

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-from .models import Comment, VideoUrl, PlaylistUrl
+from .models import VideoUrl, PlaylistUrl
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
